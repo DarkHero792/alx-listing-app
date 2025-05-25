93c403bc45e95a1c0f79ef23988232fbfa902bc2
Abdelhamid_Marmar <abdelhamidmarmar4@gmail.com>
2025-05-25 03:06:58 +0100

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const API_BASE_URL = 'https://api.example.com';
+export const DEFAULT_IMAGE = '/assets/placeholder.png';
+
+export const UI_TEXT = {
+  bookNow: 'Book Now',
+  seeDetails: 'See Details',
+};
